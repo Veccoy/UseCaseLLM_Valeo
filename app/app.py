333193f6224f59ef52735df9734e02d39d150947
Victor Bercy <victor.bercy@imt-atlantique.net>
2024-09-02 22:26:12 +0200

    Add model for app and correct inference parameters

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ import torch
 app = Flask(__name__)
 
 model_name = "t5-small"
-model_ckpt = "./t5_summarizer"
+model_ckpt = "./summarizer"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -31,7 +31,7 @@ def summarize():
             input_text = prefix + inputtext
 
             tokenized_text = tokenizer.encode(input_text, return_tensors='pt', max_length=512).to(device)
-            summary_ = model.generate(tokenized_text, min_length=30, max_length=300)
+            summary_ = model.generate(tokenized_text, min_length=10, max_length=40)
             summary = tokenizer.decode(summary_[0], skip_special_tokens=True)
 
         return render_template("output.html", data={"summary": summary})
